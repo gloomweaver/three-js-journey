500b080b0a969aab9b082dd87e66c1611c4f3018
Kirill Khramkov <hramkir@gmail.com>
2024-09-29 18:48:42 +0400

    textures apply

--- src/pages/first.ts
+++ src/pages/first.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
-import gsap from "gsap";
-import { OrbitControls, TransformControls } from "three/examples/jsm/Addons.js";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+import doorColorJPG from "../textures/door/color.jpg";
 
 export function run() {
   const scene = new THREE.Scene();
@@ -9,10 +10,26 @@ export function run() {
     throw new Error("Canvas not found");
   }
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const manager = new THREE.LoadingManager();
+  const textureLoader = new THREE.TextureLoader(manager);
+  const textureDoorColor = textureLoader.load(doorColorJPG.src);
+
+  textureDoorColor.colorSpace = THREE.SRGBColorSpace;
+
+  const material = new THREE.MeshBasicMaterial({ map: textureDoorColor });
+
+  const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.5, 0.4, 16, 100),
+    material
+  );
+  torus.position.x = -2;
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+  );
+  sphere.position.x = 2;
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+  scene.add(torus, sphere, plane);
 
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -20,10 +37,10 @@ export function run() {
     0.1,
     1000
   );
-  camera.position.z = 5;
+  camera.position.z = 6;
   scene.add(camera);
 
-  const renderer = new THREE.WebGLRenderer({ canvas });
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   const controls = new OrbitControls(camera, renderer.domElement);
@@ -43,8 +60,19 @@ export function run() {
 
   window.addEventListener("resize", handleResize);
 
+  const clock = new THREE.Clock();
   function animate() {
     controls.update();
+
+    const elapsedTime = clock.getElapsedTime();
+
+    torus.rotation.y = 0.1 * elapsedTime;
+    sphere.rotation.y = 0.1 * elapsedTime;
+    plane.rotation.y = 0.1 * elapsedTime;
+
+    torus.rotation.x = -0.1 * elapsedTime;
+    sphere.rotation.x = -0.1 * elapsedTime;
+    plane.rotation.x = -0.1 * elapsedTime;
 
     renderer.render(scene, camera);
 
